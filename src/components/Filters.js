490db085663b42cc9dcf5f1242fee27572a60467
github username <github email address>
2018-04-24 18:17:55 -0400

    Done.

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -3,6 +3,9 @@ import React from 'react';
 class Filters extends React.Component {
   constructor() {
     super();
+    this.state={
+
+    }
   }
 
   render() {
@@ -10,7 +13,7 @@ class Filters extends React.Component {
       <div className="ui form">
         <h3>Animal type</h3>
         <div className="field">
-          <select name="type" id="type">
+          <select name="type" id="type" onChange = {this.props.onChangeType}>
             <option value="all">All</option>
             <option value="cat">Cats</option>
             <option value="dog">Dogs</option>
@@ -19,7 +22,7 @@ class Filters extends React.Component {
         </div>
 
         <div className="field">
-          <button className="ui secondary button">Find pets</button>
+          <button className="ui secondary button" onClick = {this.props.onFindPetsClick}>Find pets</button>
         </div>
       </div>
     );
--- src/components/Pet.js
+++ src/components/Pet.js
@@ -3,24 +3,26 @@ import React from 'react';
 class Pet extends React.Component {
   constructor() {
     super();
+    this.state = {
+
+    }
   }
 
   render() {
     return (
       <div className="card">
         <div className="content">
-          <a className="header">Pet name (gender: ♂ or ♀)</a>
+          <a className="header">{this.props.name} {this.props.gender === "male" ? '♂' : '♀'}</a>
           <div className="meta">
-            <span className="date">Pet type</span>
+            <span className="date">{this.props.type}</span>
           </div>
           <div className="description">
-            <p>Age: </p>
-            <p>Weight: </p>
+            <p>Age: {this.props.age}</p>
+            <p>Weight: {this.props.weight}</p>
           </div>
         </div>
         <div className="extra content">
-          <button className="ui primary button">Adopt pet</button>
-          <button className="ui disabled button">Already adopted</button>
+          { !this.props.isAdopted ? <button id={this.props.name} className="ui primary button" onClick = {this.props.onAdoptPet}>Adopt pet</button> :  <button id={this.props.name + "-adopted"}className="ui disabled button">Already adopted</button>}
         </div>
       </div>
     );
